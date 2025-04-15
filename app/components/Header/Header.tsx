5a12184712d3241d987e46e560eae8d2d21859b5
giorgitsintsadzee <giorgi.tsintsadze26@gmail.com>
2025-04-16 03:13:40 +0400

    fix responsive

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -4,21 +4,32 @@ const Header = () => {
     return (
         <div className={styles.conteiner}>
             <div className={styles.logo}>
-                <img src="images/logo.svg" alt="logo" />
-                <div>
-                    oevrview
+                <img src="/images/logo.svg" alt="logo" />
+                <div className={styles.ovwerview}>
+                    Overview
+                </div>
+                <div className={styles.mobile}>
+                    <img src="/images/icon.svg" alt="profile" className={styles.profile} />
                 </div>
             </div>
             <div className={styles.options}>
                 <div className={styles.searchBar}>
-                    <input type='search' placeholder='search for something' />
+                    <input type='search' placeholder='search for something' className={styles.searchInput} />
                     <div className={styles.icon}>
-                        <img src="images/search.svg" alt="search icon" />
+                        <img src="/images/search.svg" alt="search icon" />
                     </div>
                 </div>
-                <div>seeting</div>
-                <div>alert</div>
-                <div>profile</div>
+                <div className={styles.icons}>
+                    <div className={styles.setting}>
+                        <img src="/images/settings.svg" alt="setting icon" />
+                    </div>
+                    <div className={styles.setting}>
+                        <img src="/images/notification.svg" alt="notifications icon" />
+                    </div>
+                    <div>
+                        <img src="/images/icon.svg" alt="profile" className={styles.profile} />
+                    </div>
+                </div>
             </div>
         </div>
     )
